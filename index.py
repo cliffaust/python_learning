0bbd1dc1f790294b351596ae177a01469c6eb4ff
Clifford <kingsleyclifford2016@gmail.com>
2021-10-02 21:32:54 +0000

    added lines of code

---
+++ index.py
@@ -0,0 +1,26 @@
+''' Immutable and mutable Objects '''
+
+#immutable tuple
+
+a = (2, 3)
+
+# mutable string
+
+# should throw an error
+# a[0] = 1
+
+
+b = [2, 3, 4]
+
+b[1] = 10
+
+# Value printed should be an updated version of the list
+# print(b)
+
+
+#immutable string
+
+c = "Clifford"
+
+# this should throw an error, since strings are immutable
+# c[2] = "r"
